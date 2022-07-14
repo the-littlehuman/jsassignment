15a38e91fc419a39e40ed777afbe1f885564311e
Hyelhirra Samaila Malgwi <97382161+the-littlehuman@users.noreply.github.com>
2022-07-14 15:44:17 +0000

    challenge -4

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+  }
+  return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNumbers= 0;
+  for(let i = 0; i < arrayOfNumbers.length; i++) {
+    if(arrayOfNumbers[i] % 2 === 0) {
+      evenNumbers++;
+    }
+  }
+    return evenNumbers
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,13 +57,23 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  return arrayOfNumbers.map(
+    function(cels) {
+      return Math.trunc(cels * 9 / 5 + 32)
+    }
+  );
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+
+var arrayOfNumbers = [22, 89, 100];
+var fah = celsiusToFahrenheit(arrayOfNumbers);
+
+console.log(fah);
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
-// ========================
+
+// // ========================
 // DO NOT EDIT THIS BLOCK
 module.exports = assignment;
-// ========================
+// // ========================
 
